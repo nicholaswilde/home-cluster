a15f7ff307196434dcb527ea47950fc15b69b54a
Nicholas Wilde <ncwilde43@gmail.com>
2021-03-29 22:06:28 -0700

    Fix validate
    Signed-off-by: Nicholas Wilde <ncwilde43@gmail.com>

--- hack/validate.sh
+++ hack/validate.sh
@@ -17,7 +17,6 @@
 set -o errexit
 
 # mirror kustomize-controller build options
-kustomize_flags="--enable_kyaml=false --allow_id_changes=false --load_restrictor=LoadRestrictionsNone"
 kustomize_config="kustomization.yaml"
 
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
@@ -29,7 +28,9 @@ done
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" "$kustomize_flags" | kubeval --ignore-missing-schemas
+    kustomize build "${file/%$kustomize_config}" --enable_kyaml=false \
+    --allow_id_changes=false --load_restrictor=LoadRestrictionsNone | \
+    kubeval --ignore-missing-schemas
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
